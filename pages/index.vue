5f7d530f099dd0afef70452f8017e7df0c699a1c
supernovamac <supernovamac@SUPERNOVAMACBOOK.local>
2022-10-06 18:45:32 +0700

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id=navbar >
+   
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+     colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      violet: colors.violet,
+    },
+  },
+  plugins: [],
+}
